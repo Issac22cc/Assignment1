871f4e27878831041ec08a54086394c9feaee329
Issac Leung <lleung49@my.centennialcollege.ca>
2023-06-08 10:10:40 -0600

    Second commit

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,7 +1,7 @@
-//File name:
-//Student name:
-//Student ID:
-//Date:
+// File name: index.server.routes.js
+// Student name: Lok Fung Leung
+// Student ID: 301267121
+// Date: 7-6-2023 
 
 var express = require('express');
 var router = express.Router();
